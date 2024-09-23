e6fd5aa59981ab44e232ab71c56d6df95838e4fd
Emilreitinger <emil.reitinger@protonmail.com>
2024-09-23 12:31:59 +0200

    done

--- 00_GettingStarted/FirstProgramm/Program.cs
+++ 00_GettingStarted/FirstProgramm/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FirstProgram;
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 50;
+        int second = 2;
+        string? input;
+        Console.WriteLine($"Number: {number}, {second}");
+
+        input = Console.ReadLine();
+        Console.ReadLine($"Number: {input}");
+
+        if(number == 10) {
+            //TODO
+        } else if {
+            
+        }
+    }
+}
